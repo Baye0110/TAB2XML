e3667360b118292daf516a9285137865774b8286
MahdiarShoraka <74324064+MahdiarShoraka@users.noreply.github.com>
2022-02-09 10:40:22 -0500

    Adds all necessary JUnit Classes for component_data

---
+++ src/test/java/component_data_test/MeasureTest.java
@@ -0,0 +1,15 @@
+package component_data_test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+//Testing Measure.java in component_data
+class MeasureTest {
+
+	@Test
+	void testMeasure1() {
+		//complete
+	}
+
+}
