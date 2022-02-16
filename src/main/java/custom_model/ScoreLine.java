1e4994ede1650f4cae0986ee69cf3b53187056fd
prabjot09 <77180065+prabjot09@users.noreply.github.com>
2022-02-16 13:32:48 -0500

    Adding Classes to Implement Sheet Music Graphics for Drums

---
+++ src/main/java/custom_model/ScoreLine.java
@@ -0,0 +1,40 @@
+package custom_model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Line;
+
+public class ScoreLine extends HBox{
+	List<Double> measureDistances;
+	
+	public ScoreLine(List<TabMeasure> measures, double pageWidth) {
+		this.measureDistances = new ArrayList<>();
+		
+		Line start = new Line();
+		start.setStartX(0);
+		start.setStartY(measures.get(0).barLines.get(0).getStartY());
+		start.setEndX(0);
+		start.setEndY(measures.get(0).barLines.get(measures.get(0).barLines.size()-2).getEndY());
+		start.setStrokeWidth(2);
+		measures.get(0).getChildren().add(start);
+		
+		
+		double current = 0;
+		double spacing = 0;
+		for (TabMeasure m: measures) {
+			this.getChildren().add(m);
+			this.measureDistances.add(current);
+			current += m.minWidth;
+			spacing += m.spacing; 
+		}
+		
+		double scale = 1 + (pageWidth - current) / spacing;
+		System.out.println(scale);
+		for (TabMeasure m: measures) {
+			m.setBaseDistance(scale);
+			m.setSpacing(scale);
+		}
+	}
+}
