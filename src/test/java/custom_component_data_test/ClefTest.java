e67537ed7297caef31c91040e215b085bcb35f9c
Hieu Le <hieule10@my.yorku.ca>
2022-02-19 16:17:23 -0500

    First 5 tests for Clef class

--- src/test/java/custom_component_data_test/ClefTest.java
+++ src/test/java/custom_component_data_test/ClefTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import org.junit.jupiter.api.Assertions;
 //import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
@@ -15,13 +16,8 @@ import custom_component_data.Score;
 //Testing Clef.java 
 
 class ClefTest {
-	@Test
-	public void cleftTest1() {
-		Clef c1 = new Clef('G',1);
-		assertNotNull(c1);
-	}
 	
-	Score score = null;
+	//Score score = null;
 	
 	/*@Before
 	public void setUp() {
@@ -52,19 +48,52 @@ class ClefTest {
         
 	}*/
 	
+	@Test
+	public void cleftTest1() {
+		Clef c1 = new Clef('G',1);
+		assertNotNull(c1);
+	}
+	
 	
 	@Test
 	public void cleftTest2() {
+		Score scoreSheet = new Score("capricho.txt");
 		Clef expected = new Clef('G', 2);
-		Clef actual = score.getParts().get(0).getMeasures().get(0).getClef();
+		Clef actual = scoreSheet.getParts().get(0).getMeasures().get(0).getClef();
+		assertNotNull(actual);
 		assertEquals(expected, actual);
+		
+		
 	}
 	
 	@Test
 	public void clefTest3() {
-		
+		Score scoreSheet = new Score("capricho.txt");
+		Clef expected = new Clef('G', 2);
+		Clef actual1 = scoreSheet.getParts().get(0).getMeasures().get(0).getClef();
+		Clef actual2 = scoreSheet.getParts().get(0).getMeasures().get(3).getClef();
+		assertNotNull(actual1);
+		assertNotNull(actual2);
+		Assertions.assertTrue(actual1.getSymbol() == actual2.getSymbol(), "Measure 1 has Clef " + actual1.getSymbol() + "while Measure 2 has Clef " + actual2.getSymbol());
 	}
 	
+	
+	@Test
+	public void clefTest4() {
+		Score scoreSheet = new Score("ex38.txt");
+		Clef expected = new Clef('G', 2);
+		Clef actual = scoreSheet.getParts().get(0).getMeasures().get(0).getClef();
+		assertNotNull(actual);
+		assertEquals(expected, actual); 
+	}
+	
+	@Test
+	public void clefTest5() {
+		Score scoreSheet = new Score("ex38.txt");
+		Clef expected = new Clef('G', 2);
+		Clef actual = scoreSheet.getParts().get(0).getMeasures().get(0).getClef();
+		Assertions.assertTrue(expected.getLine() == actual.getLine(), "The expected line of clef is " + expected.getLine() + "while the actual line is " + actual.getLine()); 
+	}
 	
 }
 
