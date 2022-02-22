eeac875ee4e5add4ef1e5328c72de3e47fc0d32a
Hieu Le <hieule10@my.yorku.ca>
2022-02-22 09:41:25 -0500

    Create 2 tests for Part class

--- src/test/java/custom_component_data_test/PartTest.java
+++ src/test/java/custom_component_data_test/PartTest.java
@@ -2,13 +2,92 @@ package custom_component_data_test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import custom_component_data.Score;
+
 class PartTest {
+	
+	Score score = null;
+	
+	private void setUp(String location) {
+		 // Get the file
+        File in = new File(location);
+        String build = "";
+
+        // Set up Scanner to use File Input (If invalid file then no Score object will be created
+        Scanner input = null;
+        try {
+            input = new Scanner(in);
+            input.useDelimiter("\n");
+            
+            // Put the entire file in String "build"
+            while(input.hasNext()) {
+                build += input.next() + "\n";
+            }
+            score = new Score(build);
+
+        } 
+        catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void partTest1() {
+		setUp("src/test/resources/system/demoDrumsSimple1.musicxml");
+		int partCount = 0;
+		for(int i = 0; i < score.getParts().size(); i++) {
+			partCount++;
+		}
+		Assertions.assertEquals(1, partCount);
+		Assertions.assertTrue(score.getParts().get(0).getId().equals("P1"));
+		Assertions.assertTrue(score.getParts().get(0).getName().equals("Drumset"));
+		
+		int measureCount = 0;
+		for(int i = 0; i < score.getParts().get(0).getMeasures().size(); i++) {
+			measureCount++;
+		}
+		Assertions.assertEquals(3, measureCount);
+		
+		int instrumentCount = 0;
+		for(int i = 0; i < score.getParts().get(0).getInstruments().size(); i++) {
+			instrumentCount++;
+		}
+		Assertions.assertEquals(13, instrumentCount);
 	}
+	
+	@Test
+	public void partTest2() {
+		setUp("src/test/resources/system/demoBassSimple1.musicxml");
+		int partCount = 0;
+		for(int i = 0; i < score.getParts().size(); i++) {
+			partCount++;
+		}
+		Assertions.assertEquals(1, partCount);
+		Assertions.assertTrue(score.getParts().get(0).getId().equals("P1"));
+		Assertions.assertFalse(score.getParts().get(0).getName().equals("Drumset"));
+		Assertions.assertFalse(score.getParts().get(0).getName().equals("Lololol"));
+		Assertions.assertTrue(score.getParts().get(0).getName().equals("Bass"));
+		
+		int measureCount = 0;
+		for(int i = 0; i < score.getParts().get(0).getMeasures().size(); i++) {
+			measureCount++;
+		}
+		Assertions.assertEquals(4, measureCount);
+		
+		int instrumentCount = 0;
+		for(int i = 0; i < score.getParts().get(0).getInstruments().size(); i++) {
+			instrumentCount++;
+		}
+		Assertions.assertEquals(0, instrumentCount);
+	}
+	
 
 }
