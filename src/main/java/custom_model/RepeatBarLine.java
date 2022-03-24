b0b5f9b9769c829aba00d786222a326f6232aa50
prabjot09 <77180065+prabjot09@users.noreply.github.com>
2022-03-24 16:36:11 -0400

    Document the changes to RepeatBarLine.java

--- src/main/java/custom_model/RepeatBarLine.java
+++ src/main/java/custom_model/RepeatBarLine.java
@@ -11,7 +11,8 @@ public class RepeatBarLine extends Group {
 	
 	//constructor of gui element
 	RepeatBarLine(int lineSpacing, int numOfLines) {
-		int n = numOfLines * lineSpacing;
+		// New Addition: Adjust this value to what it was before the deletion of the for loop for unnecessary horizontal lines
+		int n = 50 + (numOfLines * lineSpacing);
 		
 		//drawing vertical lines
 		Line v1 = new Line();
@@ -21,7 +22,7 @@ public class RepeatBarLine extends Group {
 		v1.setEndY(n- lineSpacing);
 		v1.setStrokeWidth(2);
 		
-		this.getChildren().add(v1);
+		this.getChildren().add(v1);  // CHANGE: This line is added to the Class directly now
 		
 		//the second line has a higher Stroke Width
 		Line v2 = new Line();
@@ -31,7 +32,7 @@ public class RepeatBarLine extends Group {
 		v2.setEndY(n - lineSpacing);
 		v2.setStrokeWidth(5);
 		
-		this.getChildren().add(v2);
+		this.getChildren().add(v2); // CHANGE: This line is added to the Class directly now
 		
 		//drawing the first dot (upper dot)
 		 Circle circle = new Circle();
@@ -40,7 +41,7 @@ public class RepeatBarLine extends Group {
 		 circle.setRadius(5);
 		 circle.setFill(Color.BLACK);
 
-		 this.getChildren().add(circle);
+		 this.getChildren().add(circle); // CHANGE: This line is added to the Class directly now
 		 
 		 //drawing the second dot
 		 Circle circle2 = new Circle();
@@ -49,7 +50,7 @@ public class RepeatBarLine extends Group {
 		 circle2.setRadius(5);
 		 circle2.setFill(Color.BLACK);
 		 
-		 this.getChildren().add(circle2);
+		 this.getChildren().add(circle2); // CHANGE: This line is added to the Class directly now
 	}
 	
 			
