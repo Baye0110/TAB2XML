eb4efff482c02ddaf2f423c58a02577e6ffee391
Yongjie Ba <baye0110@gmail.com>
2022-03-27 16:38:50 -0400

    nothing

---
+++ src/main/java/custom_model/DisplayUnit.java
@@ -0,0 +1,31 @@
+package custom_model;
+
+import javafx.scene.Group;
+
+// DRUMS ONLY
+public abstract class DisplayUnit extends NoteUnit{
+	// ABSTRACT CLASS for the DisplayNote, and DisplayChord
+	
+	// All the instance variables which are common to both DisplayNote and DisplayChord
+	double top;
+	double bottom;
+	double height;
+	int position;
+	double grace;
+	
+	public double getWidth() {
+		return this.width;
+	}
+	
+	public int getPosition() {
+		return this.position;
+	}
+	
+	public double getSpacingType() {
+		return this.spacingType;
+	}
+
+	public abstract void extendStaff(int positions, double height);
+	
+	public abstract void addTails(double height, boolean stemDown);
+}
