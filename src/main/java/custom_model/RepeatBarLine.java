09f5058ca77a496015a624dc8c78f56bd0e13847
MahdiarShoraka <mahdiarshoraka@gmail.com>
2022-03-31 10:32:19 -0400

    Modifications for RepeatBarLine.java

--- src/main/java/custom_model/RepeatBarLine.java
+++ src/main/java/custom_model/RepeatBarLine.java
@@ -11,8 +11,8 @@ public class RepeatBarLine extends Group {
 	
 	//constructor of gui element
 	RepeatBarLine(int lineSpacing, int numOfLines) {
-		// New Addition: Adjust this value to what it was before the deletion of the for loop for unnecessary horizontal lines
-		int n = 50 + (numOfLines * lineSpacing);
+		
+		int n = 0 + numOfLines * lineSpacing;
 		
 		//drawing vertical lines
 		Line v1 = new Line();
@@ -22,35 +22,35 @@ public class RepeatBarLine extends Group {
 		v1.setEndY(n- lineSpacing);
 		v1.setStrokeWidth(2);
 		
-		this.getChildren().add(v1);  // CHANGE: This line is added to the Class directly now
+		this.getChildren().add(v1);  
 		
 		//the second line has a higher Stroke Width
 		Line v2 = new Line();
-		v2.setStartX(60);
-		v2.setStartY(50);
-		v2.setEndX(60);
+		v2.setStartX(0);
+		v2.setStartY(0);	
+		v2.setEndX(0);	
 		v2.setEndY(n - lineSpacing);
-		v2.setStrokeWidth(5);
+		v2.setStrokeWidth(1/3* lineSpacing);
 		
-		this.getChildren().add(v2); // CHANGE: This line is added to the Class directly now
+		this.getChildren().add(v2); 
 		
 		//drawing the first dot (upper dot)
 		 Circle circle = new Circle();
 		 circle.setCenterX(85);
-		 circle.setCenterY((50 + (50 + lineSpacing))/2);
-		 circle.setRadius(5);
+		 circle.setCenterY(lineSpacing/2);	
+		 circle.setRadius(1/6*lineSpacing);
 		 circle.setFill(Color.BLACK);
 
-		 this.getChildren().add(circle); // CHANGE: This line is added to the Class directly now
+		 this.getChildren().add(circle); 
 		 
 		 //drawing the second dot
 		 Circle circle2 = new Circle();
 		 circle2.setCenterX(85);
 		 circle2.setCenterY(2*lineSpacing + (50 + (50 + lineSpacing))/2);
-		 circle2.setRadius(5);
+		 circle2.setRadius(1/6*lineSpacing);
 		 circle2.setFill(Color.BLACK);
 		 
-		 this.getChildren().add(circle2); // CHANGE: This line is added to the Class directly now
+		 this.getChildren().add(circle2);
 	}
 	
 			
