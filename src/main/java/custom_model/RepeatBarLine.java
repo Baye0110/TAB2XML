534cf43b13eb9210f1319fbaf819c507b7f80d47
MahdiarShoraka <mahdiarshoraka@gmail.com>
2022-04-01 09:12:53 -0400

    RepeatBarLine Modifications

--- src/main/java/custom_model/RepeatBarLine.java
+++ src/main/java/custom_model/RepeatBarLine.java
@@ -16,18 +16,18 @@ public class RepeatBarLine extends Group {
 		
 		//drawing vertical lines
 		Line v1 = new Line();
-		v1.setStartX(75);
+		v1.setStartX(10);
 		v1.setStartY(50);
-		v1.setEndX(75);
+		v1.setEndX(10);
 		v1.setEndY(n- lineSpacing);
-		v1.setStrokeWidth(2);
+		v1.setStrokeWidth(1/6*lineSpacing);
 		
 		this.getChildren().add(v1);  
 		
 		//the second line has a higher Stroke Width
 		Line v2 = new Line();
 		v2.setStartX(0);
-		v2.setStartY(0);	
+		v2.setStartY(50);	
 		v2.setEndX(0);	
 		v2.setEndY(n - lineSpacing);
 		v2.setStrokeWidth(1/3* lineSpacing);
@@ -36,7 +36,7 @@ public class RepeatBarLine extends Group {
 		
 		//drawing the first dot (upper dot)
 		 Circle circle = new Circle();
-		 circle.setCenterX(85);
+		 circle.setCenterX(20);
 		 circle.setCenterY(lineSpacing/2);	
 		 circle.setRadius(1/6*lineSpacing);
 		 circle.setFill(Color.BLACK);
@@ -45,8 +45,8 @@ public class RepeatBarLine extends Group {
 		 
 		 //drawing the second dot
 		 Circle circle2 = new Circle();
-		 circle2.setCenterX(85);
-		 circle2.setCenterY(2*lineSpacing + (50 + (50 + lineSpacing))/2);
+		 circle2.setCenterX(20);
+		 circle2.setCenterY((50 - (n-lineSpacing) - lineSpacing)/2);	//(vertical line height - lineSpacing)/2
 		 circle2.setRadius(1/6*lineSpacing);
 		 circle2.setFill(Color.BLACK);
 		 
