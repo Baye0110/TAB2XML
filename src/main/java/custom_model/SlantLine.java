a58dae250071702971ba40bf3c97276a8555b979
Hieu Le <hieule10@my.yorku.ca>
2022-04-02 15:48:23 -0400

    add ability to flip side for slant class

--- src/main/java/custom_model/SlantLine.java
+++ src/main/java/custom_model/SlantLine.java
@@ -8,7 +8,7 @@ public class SlantLine extends Group {
 	
 	// spacing between 2 connected notes = slant length
 	// height difference between 2 connected notes = x-coordinate rotation degree
-	public SlantLine(double slantLength, double slantWidth) {
+	public SlantLine(double slantLength, double slantWidth, String rotation) {
 		
 		Polygon slant = new Polygon();
 		slant.getPoints().addAll(new Double[] {
@@ -17,6 +17,10 @@ public class SlantLine extends Group {
 				100.0 + slantLength, 70.0 + slantWidth,
 				100.0 + slantLength, 70.0,
 		});
+		
+		if(rotation.equals("right")) {
+			slant.setScaleX(-1);
+		}
 		this.getChildren().add(slant);
 		
 	}
