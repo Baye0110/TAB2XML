e8e1f33f12ecdf5495be26868fae641e53b00a79
Hieu Le <hieule10@my.yorku.ca>
2022-04-05 11:28:08 -0400

    .

--- src/main/java/custom_model/Tremolo.java
+++ src/main/java/custom_model/Tremolo.java
@@ -1,5 +1,9 @@
 package custom_model;
 
 public class Tremolo {
+	public Tremolo(double slantLength, double slantWidth, boolean rotation, int numLines) {
+		SlantLine sl1 = new SlantLine(slantLength, slantWidth, false);
+		
+	}
 
 }
